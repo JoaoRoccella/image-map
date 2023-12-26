aa67ce81e8c8a739d54fae57bbc617a6e27bba40
JoaoRoccella <roccella@gmail.com>
2023-12-26 10:35:49 -0300

    feat: botão 'close'

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 document.querySelector('#area-parabrisa').addEventListener('click', () => {
-    document.querySelector('#parabrisa').classList.toggle('oculto');
-    document.querySelector('#parabrisa').classList.toggle('data');
+    document.querySelector('#parabrisa').classList.remove('oculto');
+    document.querySelector('#parabrisa').classList.add('data');
+});
+
+document.addEventListener('click', e => {
+    if (e.target.classList.contains('close')) {
+        e.target.closest('div').classList.add('oculto');
+    }
 });
