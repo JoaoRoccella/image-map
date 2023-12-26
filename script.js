bf1254e0f0a967afb5104f5b81e93a7ba7d532a9
JoaoRoccella <roccella@gmail.com>
2023-12-26 12:00:10 -0300

    feat: fecha infos com ESC

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Exibe uma determinada div de informação, baseada na área da imagem em que foi clicada
 document.addEventListener('click', e => {
 
     if (e.target.localName === 'area') {
@@ -10,12 +11,24 @@ document.addEventListener('click', e => {
 
         document.getElementById(e.target.getAttribute('data-target')).classList.remove('oculto');
 
+    // caso contrário, ao clicar no X da div fecha o conteúdo
+    } else if (e.target.classList.contains('close')) {
+        
+        e.target.closest('div').classList.add('oculto');
+
     }
 
 });
 
-document.addEventListener('click', e => {
-    if (e.target.classList.contains('close')) {
-        e.target.closest('div').classList.add('oculto');
+// Fecha qualquer div de informação pressionando ESC
+document.addEventListener('keydown', e => {
+
+    if (e.key === 'Escape') {
+
+        document.querySelectorAll('.data').forEach(dataDiv => {
+            dataDiv.classList.add('oculto');
+        })
+
     }
+
 });
