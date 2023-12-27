0419433af3c33a2535fbac5b99dfc9a4077292de
Home <joao@home>
2023-12-26 23:36:00 -0300

    feat: highlight peças do carro

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 ['load', 'resize'].forEach(event => {
 
     addEventListener(event, () => {
-        document.querySelector('#overlay').style.height =  document.querySelector('img').clientHeight + 'px';
+        document.querySelector('#overlay').style.height =  document.querySelector('img#imagemBase').clientHeight + 'px';
     });
 
 });
@@ -14,17 +14,37 @@ document.addEventListener('click', e => {
 
         document.querySelectorAll('div.data').forEach(dataDiv => {
             dataDiv.classList.add('oculto');
-            document.querySelector('#overlay').classList.add('oculto');
         });
+        
+        document.querySelector('#overlay').classList.add('oculto');
 
         document.getElementById(e.target.getAttribute('data-target')).classList.remove('oculto');
+
+        document.querySelector(`img[data-target="${e.target.getAttribute('data-target')}"`).classList.remove('oculto');
 
         document.querySelector('#overlay').classList.remove('oculto');
 
     // caso contrário, ao clicar no X da div fecha o conteúdo
     } else if (e.target.classList.contains('close')) {
         
+        document.querySelectorAll('.sobreposicao').forEach(imagem => {
+            imagem.classList.add('oculto');
+        });
+
         e.target.closest('div').classList.add('oculto');
+
+        document.querySelector('#overlay').classList.add('oculto');
+
+    } else if (e.target.classList.contains('sobreposicao')) {
+
+        document.querySelectorAll('.sobreposicao').forEach(imagem => {
+            imagem.classList.add('oculto');
+        });
+
+        document.querySelectorAll('.data').forEach(dataDiv => {
+            dataDiv.classList.add('oculto');
+        });
+
         document.querySelector('#overlay').classList.add('oculto');
 
     }
@@ -36,11 +56,24 @@ document.addEventListener('keydown', e => {
 
     if (e.key === 'Escape') {
 
+        document.querySelectorAll('.sobreposicao').forEach(imagem => {
+            imagem.classList.add('oculto');
+        });
+
         document.querySelectorAll('.data').forEach(dataDiv => {
             dataDiv.classList.add('oculto');
-            document.querySelector('#overlay').classList.add('oculto');
-        })
-
+        });
+        
+        document.querySelector('#overlay').classList.add('oculto');
     }
 
+});
+
+// Fecha qualquer div de informação ao clicar no overlay
+document.querySelector('#overlay').addEventListener('click', e => {
+    e.target.classList.add('oculto');
+
+    document.querySelectorAll('.data').forEach(dataDiv => {
+        dataDiv.classList.add('oculto');
+    });
 });
